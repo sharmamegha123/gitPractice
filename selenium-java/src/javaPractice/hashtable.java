c7d028aad8bec0e517e78f6158bdda8d903ab109
Megha Sharma <Megha.Sharma@rsystems.com>
2018-05-25 11:49:23 +0530

    array list example

---
+++ selenium-java/src/javaPractice/hashtable.java
@@ -0,0 +1,21 @@
+package javaPractice;
+
+import java.util.Hashtable;
+
+public class hashtable {
+
+	
+	static Hashtable<Integer,String> h=new Hashtable();
+	
+	public static void main(String args[])
+	{
+		h.put(1,"geeks");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ selenium-java/src/javaPractice/userdefinedArrayList.java
@@ -0,0 +1,13 @@
+package javaPractice;
+
+public class userdefinedArrayList {
+
+	int rollno;
+	String name;
+	
+	userdefinedArrayList(int rollno,String name)
+	{
+		this.rollno=rollno;
+		this.name=name;
+	}
+}
